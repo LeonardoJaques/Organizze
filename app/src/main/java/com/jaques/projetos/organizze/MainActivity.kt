803338266e8f5383fb08973f2140523c8fbc62b1
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-05 20:52:51 -0300

    Create activity_login, activity_register, intro_cadastro, LoginActivity and RegisterActivity.

--- app/src/main/java/com/jaques/projetos/organizze/MainActivity.kt
+++ app/src/main/java/com/jaques/projetos/organizze/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.jaques.projetos.organizze
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
 
@@ -10,22 +12,42 @@ class MainActivity : IntroActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
+
+
         isButtonBackVisible = false
         isButtonNextVisible = false
 
-        for (i in 0..3){
-            val fragmentIntro = arrayListOf(R.layout.intro_1,
-                                                 R.layout.intro_2,
-                                                 R.layout.intro_3,
-                                                 R.layout.intro_4 )[i]
+        for (i in 0..4) {
+            val fragmentIntro = arrayListOf(
+                R.layout.intro_1,
+                R.layout.intro_2,
+                R.layout.intro_3,
+                R.layout.intro_4,
+                R.layout.intro_cadastro
+            )[i]
 
-            addSlide( FragmentSlide.Builder()
-                .background(android.R.color.white)
-                .fragment(fragmentIntro)
-                .build()
-            )
+            if (fragmentIntro == R.layout.intro_cadastro) {
+                addSlide(
+                    FragmentSlide.Builder()
+                        .background(android.R.color.white)
+                        .canGoForward(false)
+                        .fragment(fragmentIntro)
+                        .build()
+                )
+            } else
+                addSlide(
+                    FragmentSlide.Builder()
+                        .background(android.R.color.white)
+                        .fragment(fragmentIntro)
+                        .build()
+                )
         }
 
+        fun btEnter(view: View){
+//            startActivity(Intent(this,))
+        }
+
+        fun btRegister(view: View){}
 
     }
 }
---
+++ app/src/main/java/com/jaques/projetos/organizze/activity/RegisterActivity.kt
@@ -0,0 +1,13 @@
+
+package com.jaques.projetos.organizze.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaques.projetos.organizze.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
