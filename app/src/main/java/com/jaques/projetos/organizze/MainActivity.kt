6a600bb42519ab363ad21c1765b056281d0d5384
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-05 01:13:39 -0300

    Setup project with firebase auth and realtime database

---
+++ app/src/main/java/com/jaques/projetos/organizze/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.jaques.projetos.organizze
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
