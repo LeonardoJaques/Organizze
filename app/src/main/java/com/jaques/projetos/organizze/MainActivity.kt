0877e583a23b2d672f5080e00de249a9321456b8
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-06 00:05:08 -0300

    Modify MainActivity, inserted two functions btEnter and btRegister, designed activity_login.xml , activity_register.xml and intro_cadastro.xml.

--- app/src/main/java/com/jaques/projetos/organizze/MainActivity.kt
+++ app/src/main/java/com/jaques/projetos/organizze/MainActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
 import com.heinrichreimersoftware.materialintro.slide.FragmentSlide
+import com.jaques.projetos.organizze.activity.LoginActivity
+import com.jaques.projetos.organizze.activity.RegisterActivity
 
 class MainActivity : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-
-
 
         isButtonBackVisible = false
         isButtonNextVisible = false
@@ -42,12 +43,13 @@ class MainActivity : IntroActivity() {
                         .build()
                 )
         }
+    }
 
-        fun btEnter(view: View){
-//            startActivity(Intent(this,))
-        }
+    fun btEnter(view: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
 
-        fun btRegister(view: View){}
-
+    fun btRegister(view: View) {
+        startActivity(Intent(this, RegisterActivity::class.java))
     }
 }
