af8c8ed08ba8d54b46b0d0cc2613ff224a44f472
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-10 21:04:46 -0300

    Change MajorActivity.kt create a two functions addExpense and addRevenue Inserting ic_baseline_add_24.xml image Included Floating Action Menu and Buttons in activity_major.xml Inserted in colors.xml colors for expense and revenue Inserted in strings.xml the names of expense and revenue in brazilian portuguese

--- app/src/main/java/com/jaques/projetos/organizze/activity/MajorActivity.kt
+++ app/src/main/java/com/jaques/projetos/organizze/activity/MajorActivity.kt
@@ -1,6 +1,7 @@
 package com.jaques.projetos.organizze.activity
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +12,15 @@ class MajorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_major)
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+//        setSupportActionBar(findViewById(R.id.toolbar))
+//
+//        findViewById<FloatingActionButton>(R.id.fab_expense).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
     }
+
+    fun addExpense(view: View) {}
+    fun addRevenue(view: View) {}
+
 }
