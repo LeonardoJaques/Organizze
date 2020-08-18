66f7a6ee99ddca1d6108c3aee442dc9428434688
LeonardoJaques <jaques.projetos@outlook.com>
2020-08-17 21:04:25 -0300

    Modified MajorActivity.kt setting calendar view lib and design content_major.xml, install lib in gradle.

--- app/src/main/java/com/jaques/projetos/organizze/activity/MajorActivity.kt
+++ app/src/main/java/com/jaques/projetos/organizze/activity/MajorActivity.kt
@@ -2,23 +2,55 @@ package com.jaques.projetos.organizze.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.jaques.projetos.organizze.R
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView
+import kotlinx.android.synthetic.main.content_major.*
 
 class MajorActivity : AppCompatActivity() {
+
+    private lateinit var calendar: MaterialCalendarView
+    private lateinit var textWelcome: TextView
+    private lateinit var textBalance: TextView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_major)
 //        setSupportActionBar(findViewById(R.id.toolbar))
-//
-//        findViewById<FloatingActionButton>(R.id.fab_expense).setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
+
+        textBalance = textView_Balance_Major
+        textWelcome = textWelcomeUser_Major
+        calendar = calendarView
+        settingsCalendar()
+
+
+    }
+
+    fun settingsCalendar() {
+
+
+        calendar.setTitleMonths(
+            arrayOf<CharSequence>(
+                "Janeiro", "Fevereiro", "Março", "Abril", "Maio",
+                "Junho", "Julho", "Agosto", "Setembro", "Outubro",
+                "Novembro", "Dezembro"
+            )
+        )
+
+        calendar.setWeekDayLabels(
+            arrayOf<CharSequence>(
+                "S", "T", "Q", "Q",
+                "S", "S", "D"
+            )
+        )
+
+        calendar.setOnMonthChangedListener { widget, date ->
+            Log.i("data:", " Valor 1:${date.month}/${date.year}")
+        }
     }
 
     fun addExpense(view: View) =
